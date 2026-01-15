b6c9a5c470a09c15cc2a870fd36c2edef50c9724
Bally <xmasball@g.lpru.ac.th>
2026-01-15 18:22:12 +0700

    fix: Fix map closing syntax in my-loans page

--- app/my-loans/page.tsx
+++ app/my-loans/page.tsx
@@ -322,7 +322,7 @@ export default function MyLoansPage() {
                                             </div>
                                         </div>
                                     )
-                                )})
+                                })}
                             </div>
 
                             {/* Pagination */}
